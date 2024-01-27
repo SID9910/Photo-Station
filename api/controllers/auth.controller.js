b722bd797428321385378e153611e31abeb56ddc
SIDDHARTH GUPTA <95719152+SID9910@users.noreply.github.com>
2024-01-27 11:02:21 +0530

    npm i jsonwebtoken for sigin

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -15,7 +15,7 @@ export const signup = async (req, res ,next)=>{
       next(errorHandler(400, 'All fields are required'));
     }
 
-     //used to encypt the password in  mongodb database
+     //used to encypt or hashed the password in  mongodb database
      const hashedPassword = bcryptjs.hashSync(password, 10); 
      const newUser = new User({
         username,
@@ -31,3 +31,29 @@ export const signup = async (req, res ,next)=>{
      next(error);
   }
 };
+
+
+//sign in
+
+export const signin = async (req , res, next ) =>{
+   const { email , password } = req.body;
+
+   if(!email || !password || email ==='' || password ===''){
+      next(errorHandler(400 ,'All fields are required'));
+   }
+
+   try{
+
+      const validUser = await User.findOne({email});
+      if(!validUser){
+         next(errorHandler(404,'User not found'));
+      }
+      //hashed password ko normal karke input se compare karega
+      const validpassword =bcryptjs.compareSync(password ,validUser.password);
+      if(!validpassword){
+         next(errorHandler(400,'Invalid password'));
+      }
+   }catch(error){
+      next(error)
+   }
+}
