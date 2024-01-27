abf54459a5a47e0dc9e5617e9fb639dd3fc2db2f
SIDDHARTH GUPTA <95719152+SID9910@users.noreply.github.com>
2024-01-27 13:11:22 +0530

    add redux toolkit and and implement dispatch to handleerror

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'; //used to encypt the password in  mongodb database
 import { errorHandler } from "../utils/error.js";
-
+//ye
 import  Jwt  from "jsonwebtoken";
 export const signup = async (req, res ,next)=>{
 
