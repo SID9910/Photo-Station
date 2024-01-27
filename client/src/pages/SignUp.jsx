b84e37aca6c7bbe474077368dd815e7ed4ac824a
SIDDHARTH GUPTA <95719152+SID9910@users.noreply.github.com>
2024-01-27 09:54:15 +0530

    add functionality to sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,50 +1,50 @@
-import { Link  } from "react-router-dom";
-import { Label,Button, TextInput } from "flowbite-react";
+import { Link ,useNavigate } from "react-router-dom";
+import { Label,Button, TextInput, Alert, Spinner } from "flowbite-react";
 import { useState } from "react";
 
 export default function SignUp() {
   
    const [formData , setformData] = useState({});
+    const[errorMessage , setErrorMessage] = useState(null);
+    const [loading ,setLoading] =useState(false);
 
-
+const navigate =useNavigate();
+    // trip is used to remove space before input
   const handleChange =(e)=>{
-    setformData({...formData , [e.target.id]: e.target.value});
+    setformData({...formData , [e.target.id]: e.target.value.trim()});
   };
   
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   try{
-
-  //     const res = await fetch('/api/auth/signup', {
-  //       method: 'POST',
-  //       headers: { 'Content-Type': 'application/json' },
-  //       body: JSON.stringify(formData),
-  //     });
-  //     const data = await res.json();
-
-  //   }catch(error){
-  //     console.log(error);
-  //   }
-  // };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if(!formData.username || !formData.email || !formData.password){
+      return setErrorMessage('Please fill out all the fields');
+    }
     try {
-      console.log('Form data:', formData); // Check if formData is correctly populated
-      
+         setLoading(true);
+         setErrorMessage(null);
+      // Check if formData is correctly populated
+
       const res = await fetch('/api/auth/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
       });
-      
-      console.log('Response:', res); // Check the response object
+   
       
       const data = await res.json();
-      console.log('Data:', data); // Check the parsed data
+      if(data.success ===false){
+        return setErrorMessage(data.message);
+      }
+     setLoading(false);
+     
+     if(res.ok){
+      navigate('/sign-in');
+     }
       
     } catch (error) {
-      console.error('Error:', error); // Log any errors
+      setErrorMessage(error.message); // Log any errors
+      setLoading(false);
     }
   };
   
@@ -95,14 +95,31 @@ export default function SignUp() {
                 id='password'
                 onChange={handleChange}
               />
-            </div><Button type="submit" gradientDuoTone='purpleToBlue'>Sign Up</Button>
+            </div><Button type="submit" gradientDuoTone='purpleToBlue' disabled={loading}>
+
+{
+  loading ?(
+    <>
+    <Spinner size='sm' />
+    <span className="pl-3">Loading...</span>
+    </>
+  ):'Sign Up'
+}
+
+            </Button>
   </form>
 
   <div className="flex gap-2 text-sm mt-5">
     <span>Have an account?</span>
     <Link to='/sign-in' className="text-blue-500" >Sign In</Link>
   </div>
-</div>
+
+{
+  errorMessage && (
+    <Alert className="mt-5" color='failure'>{errorMessage}</Alert>
+    )
+}
+    </div>
      </div>
 
 
