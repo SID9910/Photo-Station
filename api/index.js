4eeb9db1acc859e93a10dfbdcc2920648909e80e
SIDDHARTH GUPTA <95719152+SID9910@users.noreply.github.com>
2024-01-31 11:39:05 +0530

    npm i cookie-parser used as to update the profile changes for authentication

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
-
+import cookieParser from 'cookie-parser';
 //  add your password and cluster name in url 
 //of mongodb in .env file
 
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_DB)
 const app = express();
 
 app.use(express.json());
-
+app.use(cookieParser());
 app.listen(3000 , ()=>{
 
     console.log('Server is  running on port  3000 ');
