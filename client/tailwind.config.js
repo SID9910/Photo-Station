89f57d139969e5ec142245b5995ac69a3875c562
SIDDHARTH GUPTA <95719152+SID9910@users.noreply.github.com>
2024-02-04 13:24:05 +0530

    add show more functionality to the github

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,10 +5,14 @@ export default {
 './src/**/*.{js,ts,jsx,tsx}',
     'node_modules/flowbite-react/lib/esm/**/*.js',
   ],
+  theme:{
+    extend: {},
+  },
   plugins: [
     // ...
     // eslint-disable-next-line no-undef
     require('flowbite/plugin'),
+    require('tailwind-scrollbar')
   ],
 };
 
