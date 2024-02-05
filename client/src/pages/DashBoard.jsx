823c216e2bc72d0e3a953570d2a8a2fb7ba76122
SIDDHARTH GUPTA <95719152+SID9910@users.noreply.github.com>
2024-02-05 13:35:09 +0530

    add dashboard to the admin overview

--- client/src/pages/DashBoard.jsx
+++ client/src/pages/DashBoard.jsx
@@ -5,6 +5,7 @@ import DashProfile from '../components/DashProfile'
 import DashPosts from "../components/DashPosts";
 import DashUsers from "../components/DashUsers";
 import DashComments from "../components/DashComments";
+import DashboardComp from "../components/DashboardComp";
 export default function DashBoard() {
    const location = useLocation();
    const [tab ,setTab] =useState('');
@@ -36,6 +37,8 @@ export default function DashBoard() {
     {tab ==='users' && <DashUsers />}
     {/* comments */}
     {tab ==='comments' && <DashComments />}
+    {/* dashboard comp */}
+     {tab === 'dash' && <DashboardComp />}
     </div>;
   
 }
